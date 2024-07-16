da25e2dc988354ec0ab8722134d4ffd072111be8
Ladorar <nichoreul@hotmail.de>
2024-07-16 12:17:33 +0200

    Try oelf

--- php/hello.php
+++ php/hello.php
@@ -7,7 +7,7 @@
 <?php
 date_default_timezone_set('Europe/Berlin');
 $date = date('d.m.Y h:i:s a', time());
-echo $date, "<br>", $_SERVER['SERVER_ADDR'], "<br>", "Red ist doof123";
+echo $date, "<br>", $_SERVER['SERVER_ADDR'], "<br>", "Red ist doof1234";
 ?>
 
 </body>
