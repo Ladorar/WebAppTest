7d8a65e185b3620602cd4c1f3ef036a8bdb02827
Ladorar <nichoreul@hotmail.de>
2024-07-19 13:48:53 +0200

    wip

--- html/stuff.js
+++ html/stuff.js
@@ -1,6 +1,6 @@
 function setTime() {
     let now = new Date()
-    document.getElementById('divid').innerHTML = now.getHours() + ':' + now.getMinutes();
+    document.getElementById('divid').innerHTML = now.getHours() + ':' + now.getMinutes() + ' Uhr';
 }
 setInterval(setTime, 5000);
 
